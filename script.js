f5b1514ed37bdbfe0d6011766a9eba59e354f14f
addi110 <addi110@gmail.com>
2024-05-18 16:15:54 -0500

    Made whole page disabled when btns are clicked

--- script.js
+++ script.js
@@ -4,25 +4,30 @@ const signupX = document.getElementById('signup-x');
 const loginX = document.getElementById('login-x');
 const signupBtn = document.getElementById("signup-btn");
 const loginBtn = document.getElementById('login-btn');
+const formContnr = document.querySelector('.form-container');
 
 signupBtn.addEventListener('click', () => { 
     signupForm.classList.add('display');
     loginBtn.disabled = true;
+    formContnr.classList.add('disable');
 });
 
 signupX.addEventListener('click', () => {
     signupForm.classList.remove('display');
     loginBtn.disabled = false;
+    formContnr.classList.remove('disable');
 })
 
 loginBtn.addEventListener('click', () => {
     loginForm.classList.add('display');
     signupBtn.disabled = true;
+    formContnr.classList.add('disable');
 })
 
 loginX.addEventListener('click', () => { 
     loginForm.classList.remove('display');
     signupBtn.disabled = false;
+    formContnr.classList.remove('disable');
 })
 
 
